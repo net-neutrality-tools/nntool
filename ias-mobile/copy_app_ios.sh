5ce57f254939e8d55391f190c711a717e02c4f26
zafaco <info@zafaco.de>
2019-04-08 09:28:25 +0200

    added updated files for moving common-mobile to ias-mobile

--- ias-mobile/copy_app_ios.sh
+++ ias-mobile/copy_app_ios.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp "${PROJECT_DIR}/../common-mobile/modules/nativescript-websockets/websockets.ios.js" "${PROJECT_DIR}/../common-mobile/platforms/ios/commonmobile/app/tns_modules/nativescript-websockets/websockets.js"
-cp "${PROJECT_DIR}/../common-mobile/modules/nativescript-websockets/websockets.ios.js" "${PROJECT_DIR}/../common-mobile/node_modules/nativescript-websockets/websockets.ios.js"
+cp "${PROJECT_DIR}/../ias-mobile/modules/nativescript-websockets/websockets.ios.js" "${PROJECT_DIR}/../ias-mobile/platforms/ios/iasmobile/app/tns_modules/nativescript-websockets/websockets.js"
+cp "${PROJECT_DIR}/../ias-mobile/modules/nativescript-websockets/websockets.ios.js" "${PROJECT_DIR}/../ias-mobile/node_modules/nativescript-websockets/websockets.ios.js"
 mkdir -p "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/app"
-cp -R "${PROJECT_DIR}/../common-mobile/platforms/ios/commonmobile/app/tns_modules" "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/app"
+cp -R "${PROJECT_DIR}/../ias-mobile/platforms/ios/iasmobile/app/tns_modules" "${BUILT_PRODUCTS_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}/app"
