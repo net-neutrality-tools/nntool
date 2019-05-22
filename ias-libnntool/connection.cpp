c0a2cb99253e536fcd519429b9b583b9c089b5aa
zafaco <info@zafaco.de>
2019-05-22 17:19:56 +0200

    Added verification of TLS Certificate valid dates

--- ias-libnntool/connection.cpp
+++ ias-libnntool/connection.cpp
@@ -12,7 +12,7 @@
 
 /*!
  *      \author zafaco GmbH <info@zafaco.de>
- *      \date Last update: 2019-05-20
+ *      \date Last update: 2019-05-22
  *      \note Copyright (c) 2019 zafaco GmbH. All rights reserved.
  */
 
@@ -484,6 +484,26 @@ int CConnection::close()
 	return ::close(mSocket);
 }
 
+static int tlsVerifyCertificateCallback(int ok, X509_STORE_CTX *store_ctx)
+{
+    int cert_error = X509_STORE_CTX_get_error(store_ctx);
+
+    switch (cert_error)
+	{
+		case X509_V_ERR_CERT_NOT_YET_VALID:
+		case X509_V_ERR_ERROR_IN_CERT_NOT_BEFORE_FIELD:
+		case X509_V_ERR_CERT_HAS_EXPIRED:
+		case X509_V_ERR_ERROR_IN_CERT_NOT_AFTER_FIELD:
+		{
+			TRC_ERR("TLS Certificate Validation failed with error: " + to_string(cert_error));
+			return -1;
+			break;
+		}
+	}
+
+    return(ok);
+}
+
 int CConnection::connectTLS()
 {
   	int ssl_error = 0;
@@ -505,17 +525,33 @@ int CConnection::connectTLS()
 	}
 	else
 	{
-		string subject_name = "";
 		server_cert = SSL_get_peer_certificate (ssl);
-		subject_name = X509_NAME_oneline (X509_get_subject_name (server_cert), 0, 0);
 
-		TRC_DEBUG("TLS Certificate Subject name: " + subject_name);
-		TRC_DEBUG("TLS Subejct validation: " + mTlsSubjectValidation);
+		//check certificate
+	    int ret;
+	    X509_STORE *store;
+	    X509_STORE_CTX *store_ctx;
+
+	    store = X509_STORE_new();
+	    X509_STORE_set_verify_cb(store, tlsVerifyCertificateCallback);
+	    X509_STORE_add_cert(store, server_cert);
+
+	    store_ctx = X509_STORE_CTX_new();
+	    X509_STORE_CTX_init(store_ctx, store, server_cert, NULL);
+
+	    if (X509_verify_cert(store_ctx) != 0)
+	    {
+			return -1;
+	    }
 
 		//check subject name
+		string subject_name = "";
+		subject_name = X509_NAME_oneline (X509_get_subject_name (server_cert), 0, 0);
+		TRC_DEBUG("TLS Certificate Subject name: " + subject_name);
+		TRC_DEBUG("TLS Subejct validation: " + mTlsSubjectValidation);
 		if (mTlsSubjectValidation.compare("") != 1 && subject_name.find(mTlsSubjectValidation) == string::npos )
 		{
-			TRC_ERR("TLS Certificate Subject name validation failed ");
+			TRC_ERR("TLS Certificate Subject name validation failed");
 			return -1;
 		}
 
