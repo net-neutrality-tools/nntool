728025860dbcf1da6a70f9b27c9ca220d10459c0
Lukasz <lb@alladin.at>
2019-06-13 15:10:08 +0200

    finish interface traffic view

--- android-app/src/main/java/at/alladin/nettest/nntool/android/app/workflow/measurement/InterfaceTrafficView.java
+++ android-app/src/main/java/at/alladin/nettest/nntool/android/app/workflow/measurement/InterfaceTrafficView.java
@@ -42,8 +42,8 @@ public class InterfaceTrafficView extends LinearLayout implements InterfaceTraff
         trafficInView = new TextView[3];
         trafficOutView = new TextView[3];
         for (int i = 0; i < 3; i++) {
-            final int inId = this.getResources().getIdentifier("nerdmode_traffic_in_arrow_" + i, "id", getContext().getPackageName());
-            final int outId = this.getResources().getIdentifier("nerdmode_traffic_out_arrow_" + i, "id", getContext().getPackageName());
+            final int inId = this.getResources().getIdentifier("interface_traffic_in_arrow_" + i, "id", getContext().getPackageName());
+            final int outId = this.getResources().getIdentifier("interface_traffic_out_arrow_" + i, "id", getContext().getPackageName());
             trafficInView[i] = findViewById(inId);
             trafficOutView[i] = findViewById(outId);
         }
@@ -52,6 +52,68 @@ public class InterfaceTrafficView extends LinearLayout implements InterfaceTraff
 
     @Override
     public void onTrafficUpdate(CurrentInterfaceTraffic currentInterfaceTraffic) {
+        if (currentInterfaceTraffic != null) {
+            InterfaceTrafficClassificationEnum inClassification = InterfaceTrafficClassificationEnum.classify(currentInterfaceTraffic.getRxBps());
+            InterfaceTrafficClassificationEnum outClassification = InterfaceTrafficClassificationEnum.classify(currentInterfaceTraffic.getTxBps());
 
+            updateTrafficView(inClassification, trafficInView);
+            updateTrafficView(outClassification, trafficOutView);
+        }
     }
+
+    private void updateTrafficView(InterfaceTrafficClassificationEnum value, TextView[] trafficViews) {
+        for (int i = 0; i < 3; i++) {
+            trafficViews[i].setTextColor(getResources().getColor(R.color.interface_traffic_arrow_inactive));
+        }
+        switch(value) {
+            case HIGH:
+                trafficViews[2].setTextColor(getResources().getColor(R.color.interface_traffic_arrow_active));
+            case MID:
+                trafficViews[1].setTextColor(getResources().getColor(R.color.interface_traffic_arrow_active));
+            case LOW:
+                trafficViews[0].setTextColor(getResources().getColor(R.color.interface_traffic_arrow_active));
+            default:
+                break;
+        }
+    }
+
+    public enum InterfaceTrafficClassificationEnum {
+        UNKNOWN(Long.MIN_VALUE, Long.MIN_VALUE),
+        NONE(0,10000),  // 0 < x < 10kBit/s
+        LOW(10000,(long)1e6),   // 10k < x < 100 kBit/s
+        MID((long)1e6, (long)1e9),      // 100k < x  < 1 MBit/s
+        HIGH((long)1e9, Long.MAX_VALUE);
+
+        protected long minBps;
+        protected long maxBps;
+
+        InterfaceTrafficClassificationEnum(long minBps, long maxBps) {
+            this.minBps = minBps;
+            this.maxBps = maxBps;
+        }
+
+        public long getMinBps() {
+            return this.minBps;
+        }
+
+        public long getMaxBps() {
+            return this.maxBps;
+        }
+
+        /**
+         *
+         * @param bitPerSecond
+         * @return
+         */
+        public static InterfaceTrafficClassificationEnum classify(long bitPerSecond) {
+            for (InterfaceTrafficClassificationEnum e : InterfaceTrafficClassificationEnum.values()) {
+                if (e.getMinBps() <= bitPerSecond && e.getMaxBps() >= bitPerSecond) {
+                    return e;
+                }
+            }
+
+            return UNKNOWN;
+        }
+    }
+
 }
