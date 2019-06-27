7b7f0fad3e95261e945c211e4ded334b18f2e974
Benjamin Pucher <bp@alladin.at>
2019-06-27 17:11:42 +0200

    Create a nx and xplat workspace for website and desktop app.
    This commit creates a nx workspace for website, portal and desktop app.
    It allows us to share a code between the different apps.

---
+++ nntool-typescript/apps/web-nntool/src/app/core/core.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+// xplat
+import { NntoolCoreModule } from '@nntool-typescript/web';
+
+@NgModule({
+  imports: [NntoolCoreModule]
+})
+export class CoreModule {}
---
+++ nntool-typescript/xplat/web/features/ui/components/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+import { HeaderBaseComponent } from '@nntool-typescript/features';
+
+@Component({
+  selector: 'nntool-header',
+  templateUrl: 'header.component.html'
+})
+export class HeaderComponent extends HeaderBaseComponent {}
