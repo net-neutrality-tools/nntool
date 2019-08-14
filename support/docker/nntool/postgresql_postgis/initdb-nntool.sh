a6cb5c120d527a6acdf2817fbdee59e655d3d110
Benjamin Pucher <bp@alladin.at>
2019-08-14 17:25:52 +0200

    Improve PostgreSQL database structure.

--- support/docker/nntool/postgresql_postgis/initdb-nntool.sh
+++ support/docker/nntool/postgresql_postgis/initdb-nntool.sh
@@ -12,8 +12,15 @@ CREATE USER nntool WITH PASSWORD 'nntool';
 GRANT ALL PRIVILEGES ON DATABASE nntool to nntool;
 EOSQL
 
+export PGUSER="nntool"
+
 echo "Loading nntool related extensions into $DB"
 "${psql[@]}" --dbname="nntool" <<-'EOSQL'
+  GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO nntool;
+  GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO nntool;
+  ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT ON SEQUENCES TO nntool;
+  ALTER SCHEMA public OWNER TO nntool;
+
   CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
 EOSQL
 
