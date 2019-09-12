30ecf15e181d39f1f8a35842efb8e4f0d842c537
Benjamin Pucher <bp@alladin.at>
2019-09-12 14:48:42 +0200

    Fix config parsing of udp and tcp port tests.

--- qos-client-swift/Sources/Tasks/QoSBidirectionalIpTask.swift
+++ qos-client-swift/Sources/Tasks/QoSBidirectionalIpTask.swift
@@ -35,10 +35,14 @@ class QoSBidirectionalIpTask: QoSControlConnectionTask {
 
         if let portOut = config[CodingKeys3.portOut.rawValue]?.uint16Value {
             self.portOut = portOut
+        } else if let portOutString = config[CodingKeys3.portOut.rawValue]?.stringValue {
+            self.portOut = UInt16(portOutString)
         }
 
         if let portIn = config[CodingKeys3.portIn.rawValue]?.uint16Value {
             self.portIn = portIn
+        } else if let portInString = config[CodingKeys3.portIn.rawValue]?.stringValue {
+            self.portIn = UInt16(portInString)
         }
 
         super.init(config: config)
