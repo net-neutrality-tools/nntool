4111f3480ef23e5f62bef5c8c0b54b6179444812
Benjamin Pucher <bp@alladin.at>
2019-09-26 17:56:44 +0200

    Disable NgxLogger's support for source maps in production builds.

--- nntool-typescript/apps/web-nntool/src/app/app.module.ts
+++ nntool-typescript/apps/web-nntool/src/app/app.module.ts
@@ -37,7 +37,7 @@ const MAIN_DECLARATIONS = [AppComponent];
     LoggerModule.forRoot({
       level: NgxLoggerLevel.DEBUG,
       serverLogLevel: NgxLoggerLevel.OFF,
-      enableSourceMaps: true
+      enableSourceMaps: !environment.production
     }),
     BrowserModule,
     BrowserAnimationsModule,
