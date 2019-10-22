60995b0c9bf82265067ba932978e464316bed0ae
Lukasz Budryk <lb@alladin.at>
2019-10-22 16:50:35 +0200

    better ipv4 only selection logic

--- android-app/src/main/java/at/alladin/nettest/nntool/android/app/workflow/measurement/MeasurementService.java
+++ android-app/src/main/java/at/alladin/nettest/nntool/android/app/workflow/measurement/MeasurementService.java
@@ -188,8 +188,10 @@ public class MeasurementService extends Service implements ServiceConnection {
         final String clientIpv4 = options.getString(EXTRAS_KEY_SPEED_TASK_CLIENT_IPV4_PRIVATE);
         final String clientIpv6 = options.getString(EXTRAS_KEY_SPEED_TASK_CLIENT_IPV6_PRIVATE);
 
+        final boolean forceIpv4 = PreferencesUtil.isForceIpv4(getApplicationContext()) && clientIpv4 != null;
+
         //if ipV6 is available, use it
-        if (clientIpv6 != null && !PreferencesUtil.isForceIpv4(getApplicationContext())) {
+        if (clientIpv6 != null && !forceIpv4)  {
             Log.d(TAG, "Using IPv6...");
             speedTaskDesc.setUseIpV6(true);
             speedTaskDesc.setClientIp(clientIpv6);
