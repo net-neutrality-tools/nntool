5c254df16f547585f244648e93c8b541e0a4b825
zafaco <info@zafaco.de>
2019-11-13 17:15:01 +0100

    Changed license headers in ias-android-speed module

--- ias-android-speed/src/main/java/com/zafaco/speed/JniSpeedMeasurementResult.java
+++ ias-android-speed/src/main/java/com/zafaco/speed/JniSpeedMeasurementResult.java
@@ -1,3 +1,23 @@
+/*!
+    \file JniSpeedMeasurementResult.java
+    \author zafaco GmbH <info@zafaco.de>
+    \date Last update: 2019-11-13
+
+    Copyright (C) 2016 - 2019 zafaco GmbH
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License version 3 
+    as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.zafaco.speed;
 
 import android.support.annotation.Keep;
@@ -5,10 +25,6 @@ import android.support.annotation.Keep;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author Felix Kendlbacher (alladin-IT GmbH)
- * Complete result as reported by ias-cpp
- */
 public class JniSpeedMeasurementResult {
 
     private List<BandwidthResult> downloadInfoList = new ArrayList<>();
--- ias-android-speed/src/main/java/com/zafaco/speed/SpeedTaskDesc.java
+++ ias-android-speed/src/main/java/com/zafaco/speed/SpeedTaskDesc.java
@@ -1,10 +1,27 @@
+/*!
+    \file SpeedTaskDesc.java
+    \author zafaco GmbH <info@zafaco.de>
+    \date Last update: 2019-11-13
+
+    Copyright (C) 2016 - 2019 zafaco GmbH
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License version 3 
+    as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.zafaco.speed;
 
 import java.io.Serializable;
 
-/**
- * @author Felix Kendlbacher (alladin-IT GmbH)
- */
 public class SpeedTaskDesc implements Serializable {
 
     private String speedServerAddrV4;
