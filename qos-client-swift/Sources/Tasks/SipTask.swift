0fec1d44b1cf1db8cba19d8ce02ee2ca56a6ff09
Benjamin Pucher <bp@alladin.at>
2019-11-27 17:13:00 +0100

    Fix QoS SIP task unmarshalling on iOS.

--- qos-client-swift/Sources/Tasks/SipTask.swift
+++ qos-client-swift/Sources/Tasks/SipTask.swift
@@ -80,9 +80,43 @@ class SipTask: QoSControlConnectionTask {
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys4.self)
 
-        port = try container.decode(UInt16.self, forKey: .port)
-        count = try container.decode(Int.self, forKey: .count)
-        callDurationNs = try container.decode(UInt64.self, forKey: .callDurationNs)
+        var optionalPort = try? container.decodeIfPresent(UInt16.self, forKey: .port)
+        if optionalPort == nil {
+            if let portString = try? container.decodeIfPresent(String.self, forKey: .port) {
+                optionalPort = UInt16(portString)
+            }
+        }
+
+        var optionalCount = try? container.decodeIfPresent(Int.self, forKey: .count)
+        if optionalCount == nil {
+            if let countString = try? container.decodeIfPresent(String.self, forKey: .count) {
+                optionalCount = Int(countString)
+            }
+        }
+
+        var optionalCallDurationNs = try? container.decodeIfPresent(UInt64.self, forKey: .callDurationNs)
+        if optionalCallDurationNs == nil {
+            if let callDurationNsString = try? container.decodeIfPresent(String.self, forKey: .callDurationNs) {
+                optionalCallDurationNs = UInt64(callDurationNsString)
+            }
+        }
+
+        guard let port = optionalPort else {
+            throw ParseError.parseError("\"port\" could not be unmarshalled.")
+        }
+
+        guard let count = optionalCount else {
+            throw ParseError.parseError("\"count\" could not be unmarshalled.")
+        }
+
+        guard let callDurationNs = optionalCallDurationNs else {
+            throw ParseError.parseError("\"callDurationNs\" could not be unmarshalled.")
+        }
+
+        self.port = port
+        self.count = count
+        self.callDurationNs = callDurationNs
+
         to = try container.decode(String.self, forKey: .to)
         from = try container.decode(String.self, forKey: .from)
         via = try container.decodeIfPresent(String.self, forKey: .via)
