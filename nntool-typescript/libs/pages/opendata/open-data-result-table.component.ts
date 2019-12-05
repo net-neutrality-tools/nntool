25dd61128c77e56a971b264a82322ba9fd2a2fd7
Benjamin Pucher <bp@alladin.at>
2019-12-05 21:13:00 +0100

    Fix production build of web-nntool.

--- nntool-typescript/libs/pages/opendata/open-data-result-table.component.ts
+++ nntool-typescript/libs/pages/opendata/open-data-result-table.component.ts
@@ -7,9 +7,13 @@ import { map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { SpringServerDataSource } from '../../core/services/table/spring-server.data-source';
 import { SearchApiService } from '../../core/services/search-api.service';
 import { DateParseService } from '@nntool-typescript/core/services/date-parse.service';
-import { ProviderFilterResponse, BasicFilter, FilterType, FilterOption } from '../../core/models/provider-filter-response';
+import {
+  ProviderFilterResponse,
+  BasicFilter,
+  FilterType,
+  FilterOption
+} from '../../core/models/provider-filter-response';
 import { FormValue } from '../../core/components/dynamic-form/dynamic-form.component';
-
 
 @Component({
   templateUrl: './open-data-result-table.component.html',
@@ -71,7 +75,9 @@ export class OpenDataResultTableComponent implements OnInit {
 
   public tableSource: SpringServerDataSource;
 
-  public showFilterFields: boolean = true;
+  public showFilterFields = true;
+
+  public filterResponse: ProviderFilterResponse;
 
   @ViewChild('fullTextSearchInput', { static: true }) private fullTextSearchInput: ElementRef;
 
@@ -84,8 +90,6 @@ export class OpenDataResultTableComponent implements OnInit {
   ) {
     this.tableSource = this.searchApiService.getServerDataSource();
   }
-
-  private filterResponse: ProviderFilterResponse;
 
   public ngOnInit() {
     fromEvent(this.fullTextSearchInput.nativeElement, 'keyup')
@@ -107,18 +111,18 @@ export class OpenDataResultTableComponent implements OnInit {
         this.tableSource.refresh();
       });
 
-      this.filterResponse = this.fetchFilterResponse();
+    this.filterResponse = this.fetchFilterResponse();
   }
 
   onFormChangeCallback($event: FormValue[]) {
     if (this.tableSource && $event) {
-      let queryString: string = "";
+      let queryString: string = '';
       $event.forEach((value, index) => {
-        if (value.value && value.value !== "undefined") {
+        if (value.value && value.value !== 'undefined') {
           if (queryString.length > 0) {
-            queryString += " AND ";
+            queryString += ' AND ';
           }
-          if (value.queryString.includes("{}")) {
+          if (value.queryString.includes('{}')) {
             queryString += value.queryString.replace(/{}/g, value.value);
           } else {
             queryString += value.queryString + value.value;
@@ -175,98 +179,98 @@ export class OpenDataResultTableComponent implements OnInit {
     const ret = new ProviderFilterResponse();
     ret.filters = new Array<BasicFilter>(
       {
-        filter_type: "DROPDOWN",
-        key: "connection_type",
-        query_string: "computed_network_info.network_type_group_name:",
+        filter_type: 'DROPDOWN',
+        key: 'connection_type',
+        query_string: 'computed_network_info.network_type_group_name:',
         options: [
           { label: undefined, value: undefined },
-          { label: "LAN", value: "LAN" },
-          { label: "WLAN", value: "WLAN" },
-          { label: "2G", value: "2G" },
-          { label: "3G", value: "3G" },
-          { label: "4G", value: "4G" }
+          { label: 'LAN', value: 'LAN' },
+          { label: 'WLAN', value: 'WLAN' },
+          { label: '2G', value: '2G' },
+          { label: '3G', value: '3G' },
+          { label: '4G', value: '4G' }
         ]
       },
       {
-        filter_type: "INPUT_TEXT",
-        key: "system_uuid",
-        query_string: "system_uuid:"
+        filter_type: 'INPUT_TEXT',
+        key: 'system_uuid',
+        query_string: 'system_uuid:'
       },
       {
-        filter_type: "INPUT_NUMBER",
-        key: "download_greater",
-        query_string: "measurements.SPEED.throughput_avg_download_bps:>=",
+        filter_type: 'INPUT_NUMBER',
+        key: 'download_greater',
+        query_string: 'measurements.SPEED.throughput_avg_download_bps:>=',
         value_multiplier: 1000000
       },
       {
-        filter_type: "INPUT_NUMBER",
-        key: "download_smaller",
-        query_string: "measurements.SPEED.throughput_avg_download_bps:<=",
+        filter_type: 'INPUT_NUMBER',
+        key: 'download_smaller',
+        query_string: 'measurements.SPEED.throughput_avg_download_bps:<=',
         value_multiplier: 1000000
       },
       {
-        filter_type: "INPUT_NUMBER",
-        key: "upload_greater",
-        query_string: "measurements.SPEED.throughput_avg_upload_bps:>=",
+        filter_type: 'INPUT_NUMBER',
+        key: 'upload_greater',
+        query_string: 'measurements.SPEED.throughput_avg_upload_bps:>=',
         value_multiplier: 1000000
       },
       {
-        filter_type: "INPUT_NUMBER",
-        key: "upload_smaller",
-        query_string: "measurements.SPEED.throughput_avg_upload_bps:<=",
+        filter_type: 'INPUT_NUMBER',
+        key: 'upload_smaller',
+        query_string: 'measurements.SPEED.throughput_avg_upload_bps:<=',
         value_multiplier: 1000000
       },
       {
-        filter_type: "INPUT_NUMBER",
-        key: "rtt_greater",
-        query_string: "measurements.SPEED.rtt_info.average_ns:>=",
+        filter_type: 'INPUT_NUMBER',
+        key: 'rtt_greater',
+        query_string: 'measurements.SPEED.rtt_info.average_ns:>=',
         value_multiplier: 1000000
       },
       {
-        filter_type: "INPUT_NUMBER",
-        key: "rtt_smaller",
-        query_string: "measurements.SPEED.rtt_info.average_ns:<=",
+        filter_type: 'INPUT_NUMBER',
+        key: 'rtt_smaller',
+        query_string: 'measurements.SPEED.rtt_info.average_ns:<=',
         value_multiplier: 1000000
       },
       {
-        filter_type: "INPUT_DATE",
-        key: "date_greater",
-        query_string: "end_time:>="
+        filter_type: 'INPUT_DATE',
+        key: 'date_greater',
+        query_string: 'end_time:>='
       },
       {
-        filter_type: "INPUT_DATE",
-        key: "date_smaller",
-        query_string: "end_time:<="
+        filter_type: 'INPUT_DATE',
+        key: 'date_smaller',
+        query_string: 'end_time:<='
       },
       {
-        filter_type: "INPUT_TEXT",
-        key: "asn",
-        query_string: "computed_network_info.public_ip_asn:"
+        filter_type: 'INPUT_TEXT',
+        key: 'asn',
+        query_string: 'computed_network_info.public_ip_asn:'
       },
       {
-        filter_type: "INPUT_TEXT",
-        key: "provider",
-        query_string: "computed_network_info.network_operator_name:"
+        filter_type: 'INPUT_TEXT',
+        key: 'provider',
+        query_string: 'computed_network_info.network_operator_name:'
       },
       {
-        filter_type: "INPUT_TEXT",
-        key: "country",
-        query_string: "computed_network_info.network_country:"
+        filter_type: 'INPUT_TEXT',
+        key: 'country',
+        query_string: 'computed_network_info.network_country:'
       },
       {
-        filter_type: "INPUT_NUMBER",
-        key: "frequency",
-        query_string: "computed_network_info.frequency:"
+        filter_type: 'INPUT_NUMBER',
+        key: 'frequency',
+        query_string: 'computed_network_info.frequency:'
       },
       {
-        filter_type: "INPUT_NUMBER",
-        key: "ports_blocked_greater",
-        query_string: "qos_advanced_evaluation.blocked_ports.total_count_blocked_ports:>="
+        filter_type: 'INPUT_NUMBER',
+        key: 'ports_blocked_greater',
+        query_string: 'qos_advanced_evaluation.blocked_ports.total_count_blocked_ports:>='
       },
       {
-        filter_type: "INPUT_NUMBER",
-        key: "ports_blocked_smaller",
-        query_string: "qos_advanced_evaluation.blocked_ports.total_count_blocked_ports:<="
+        filter_type: 'INPUT_NUMBER',
+        key: 'ports_blocked_smaller',
+        query_string: 'qos_advanced_evaluation.blocked_ports.total_count_blocked_ports:<='
       }
     );
 
