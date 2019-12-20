f5613151ebf6fc7c3c95ff5b887e184a7f1940b6
Benjamin Pucher <bp@alladin.at>
2019-12-20 18:57:35 +0100

    Correct many license headers.

--- map-service/src/main/java/at/alladin/nettest/service/map/web/api/v0/docs/DocumentationResource.java
+++ map-service/src/main/java/at/alladin/nettest/service/map/web/api/v0/docs/DocumentationResource.java
@@ -1,4 +1,20 @@
-package at.alladin.nettest.service.map.web.api.v1.docs;
+/*******************************************************************************
+ * Copyright 2019 alladin-IT GmbH
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package at.alladin.nettest.service.map.web.api.v0.docs;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +38,7 @@ import at.alladin.nettest.shared.server.web.api.v1.AbstractDocumentationResource
  *
  */
 @Controller
-@RequestMapping("/api/v1/documentation")
+@RequestMapping("/api/v0/documentation")
 public class DocumentationResource extends AbstractDocumentationResource {
 
 	/*
@@ -31,7 +47,7 @@ public class DocumentationResource extends AbstractDocumentationResource {
 	 */
 	@Override
 	public String getOpenApiGroupName() {
-		return "map-service_v1";
+		return "map-service_v0";
 	}
 
 	/*
--- nettest-shared-server/src/main/java/at/alladin/nettest/shared/server/config/NettestConstants.java
+++ nettest-shared-server/src/main/java/at/alladin/nettest/shared/server/config/NettestConstants.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright 2019 alladin-IT GmbH
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
 package at.alladin.nettest.shared.server.config;
 
 /**
@@ -6,24 +22,9 @@ package at.alladin.nettest.shared.server.config;
  *
  */
 public class NettestConstants {
-
-	/**
-     * 
-     */
-    public static final String DATABASES_YAML = "databases.yml";
-    
-    /**
-     * 
-     */
-    public static final String DATABASES_YAML_CLASSPATH = "config/" + DATABASES_YAML;
 	
     /**
      * 
      */
     public static final String EXTERNAL_CONFIG_LOCATION = "/etc/nettest";
-    
-    /**
-	 * 
-	 */
-    public static final String DB_CLIENT_NAME_SUFFIX = "DbClient";
 }
