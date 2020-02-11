94809ffc1889200c00e5e31cf4af1469713f9be2
zafaco <info@zafaco.de>
2020-02-11 11:45:49 +0100

    Added multiSIM and eSim handling on iOS Home View

--- ios-app/ios-app/Sources/Utilities/NetworkInfoReachability.swift
+++ ios-app/ios-app/Sources/Utilities/NetworkInfoReachability.swift
@@ -22,7 +22,7 @@ import CoreTelephony
 class NetworkInfoReachability {
 
     private var reachability: Reachability?
-
+    // swiftlint:disable:next cyclomatic_complexity function_body_length
     init(whenReachable: ((String?, String?) -> Void)?, whenUnreachable: (() -> Void)?) {
         reachability = try? Reachability()
         reachability?.whenReachable = { r in
@@ -41,18 +41,130 @@ class NetworkInfoReachability {
 
                 networkDetailString = R.string.localizable.networkWifiTechnologyName()
             case .cellular:
+                var carrierData = [String: AnyObject]()
+                var simsActive = 0
+
+                var pSimCarrierName: String?
+                var pSimMobileCountryCode: String?
+                var pSimMobileNetworkCode: String?
+                var pSimCurrentRadioAccessTechnology: String?
+
                 let telephonyNetworkInfo = CTTelephonyNetworkInfo()
-                let carrier = telephonyNetworkInfo.subscriberCellularProvider
 
-                networkTypeString = carrier?.carrierName
+                //physical sim
+                let carrierInfo = telephonyNetworkInfo.subscriberCellularProvider!
 
-                if  let mcc = carrier?.mobileCountryCode,
-                    let mnc = carrier?.mobileNetworkCode,
-                    let currentRadioAccessTechnology = telephonyNetworkInfo.currentRadioAccessTechnology,
-                    let cellularNetworkDisplayName = NetworkInfo.getCellularNetworkTypeDisplayName(currentRadioAccessTechnology) {
+                pSimCarrierName = carrierInfo.carrierName
+                pSimMobileCountryCode = carrierInfo.mobileCountryCode
+                pSimMobileNetworkCode = carrierInfo.mobileNetworkCode
+                pSimCurrentRadioAccessTechnology = telephonyNetworkInfo.currentRadioAccessTechnology
 
-                    networkDetailString = "\(cellularNetworkDisplayName), \(mcc)-\(mnc)"
+                var pSim = [String: AnyObject]()
+
+                if pSimCarrierName == nil || pSimMobileCountryCode == nil || pSimMobileNetworkCode == nil
+                {
+                    pSim["active"] = false as AnyObject
                 }
+                if pSimCarrierName != nil {
+                    pSim["carrier"] = pSimCarrierName as AnyObject
+                }
+                if pSimMobileCountryCode != nil {
+                    pSim["mcc"] = pSimMobileCountryCode as AnyObject
+                    pSim["active"] = true as AnyObject
+                    simsActive+=1
+                }
+                if pSimMobileNetworkCode != nil {
+                    pSim["mnc"] = pSimMobileNetworkCode as AnyObject
+                }
+                if pSimCurrentRadioAccessTechnology != nil {
+                    pSim["technology"] = pSimCurrentRadioAccessTechnology as AnyObject
+                }
+
+                carrierData["sim_physical"] = pSim as AnyObject
+
+                //service subscriber cellular providers
+                let aSimArray = NSMutableArray()
+                if #available(iOS 12.0, *) {
+                    if telephonyNetworkInfo.serviceSubscriberCellularProviders!.count > 0 {
+                        simsActive = 0
+                    } else {
+                        aSimArray.add(pSim)
+                    }
+
+                    for (key, carrierInfo) in telephonyNetworkInfo.serviceSubscriberCellularProviders! {
+                        var aSimCarrierName: String?
+                        var aSimMobileCountryCode: String?
+                        var aSimMobileNetworkCode: String?
+
+                        aSimCarrierName = carrierInfo.carrierName
+                        aSimMobileCountryCode = carrierInfo.mobileCountryCode
+                        aSimMobileNetworkCode = carrierInfo.mobileNetworkCode
+
+                        var aSim = [String: AnyObject]()
+
+                        if aSimCarrierName == nil || aSimMobileCountryCode == nil || aSimMobileNetworkCode == nil {
+                            aSim["active"] = false as AnyObject
+                        }
+                        if aSimCarrierName != nil {
+                            aSim["carrier"] = aSimCarrierName as AnyObject
+                        }
+                        if aSimMobileCountryCode != nil {
+                            aSim["mcc"] = aSimMobileCountryCode as AnyObject
+                            aSim["active"] = true as AnyObject
+                            simsActive+=1
+                        }
+                        if aSimMobileNetworkCode != nil {
+                            aSim["mnc"] = aSimMobileNetworkCode as AnyObject
+                        }
+                        if telephonyNetworkInfo.serviceCurrentRadioAccessTechnology![key] != nil {
+                            aSim["technology"] = telephonyNetworkInfo.serviceCurrentRadioAccessTechnology![key] as AnyObject
+                        }
+
+                        aSimArray.add(aSim)
+                    }
+                } else {
+                    aSimArray.add(pSim)
+                }
+
+                if simsActive == 0 {
+                    NSLog("No (e)SIM Card active or no (e)SIM Card Slot present")
+                } else if simsActive > 1 {
+                    NSLog("Multiple (e)SIM Cards active")
+                }
+
+                carrierData["sims_active"] = simsActive as AnyObject
+                carrierData["sims_available"] = aSimArray as AnyObject
+
+                var currentRadioAccessTechnology: String?
+
+                for carrier in aSimArray {
+                    if let carrierDict = carrier as? [String: AnyObject] {
+                        if carrierDict["active"] as? integer_t == 1 {
+                            if networkTypeString == nil {
+                                networkTypeString = ""
+                            } else {
+                                networkTypeString! += " | "
+                            }
+                            if networkDetailString == nil {
+                                networkDetailString = ""
+                            } else {
+                                networkDetailString! += " | "
+                            }
+
+                            networkTypeString! += (carrierDict["carrier"] as? String)!
+                            networkDetailString! += (carrierDict["mcc"] as? String)! + "-" + (carrierDict["mnc"] as? String)!
+                            if carrierDict["technology"] as? String != nil {
+                                currentRadioAccessTechnology = (carrierDict["technology"] as? String)!
+                            }
+                        }
+                    }
+                }
+
+                let cellularNetworkDisplayName = NetworkInfo.getCellularNetworkTypeDisplayName(currentRadioAccessTechnology!)
+                if currentRadioAccessTechnology != nil && cellularNetworkDisplayName != nil {
+                    networkDetailString = cellularNetworkDisplayName! + ", " + networkDetailString!
+                }
+
             default:
                 break
             }
