739fd5a9678f82b1d27fac00421d62815f6c48cd
Mike Kosek <kosek@zafaco.de>
2020-03-16 11:20:07 +0100

    Removed unused variable

--- ias-server/src/udpserver.cpp
+++ ias-server/src/udpserver.cpp
@@ -1,9 +1,9 @@
 /*!
     \file udpserver.cpp
     \author zafaco GmbH <info@zafaco.de>
-    \date Last update: 2019-11-13
+    \date Last update: 2020-03-16
 
-    Copyright (C) 2016 - 2019 zafaco GmbH
+    Copyright (C) 2016 - 2020 zafaco GmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License version 3 
@@ -40,8 +40,6 @@ CUdpListener::CUdpListener(int nPort, int nIpType, string sBindIp)
 	mIpType				= nIpType;
 	mBindIp				= sBindIp;
 	
-	mClient 			= "-";
-
 	mConnection 		= std::make_unique<CConnection>();
 }
 
