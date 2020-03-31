3ec0c06c0dbe65f24252861a150b10fe4cad186d
zafaco <info@zafaco.de>
2020-03-31 19:09:44 +0200

    Added vector clearing before random data generation

--- ias-libnntool/tool.cpp
+++ ias-libnntool/tool.cpp
@@ -1,9 +1,9 @@
 /*!
     \file tool.cpp
     \author zafaco GmbH <info@zafaco.de>
-    \date Last update: 2019-11-13
+    \date Last update: 2020-03-31
 
-    Copyright (C) 2016 - 2019 zafaco GmbH
+    Copyright (C) 2016 - 2020 zafaco GmbH
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License version 3 
@@ -595,6 +595,7 @@ int CTool::randomData(vector<char> &vVector, int size)
 	// Provide seed for the random number generator.
 	srand(CTool::get_timestamp());
 	
+	vVector.clear();
 	for( int i = 0; i < size; i++ )
 		vVector.push_back( (char)randomData() );
 	
