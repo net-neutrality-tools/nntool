c9da7aa91b40a3e866aaaee68228e3a492527fcc
Lukasz Budryk <lb@alladin.at>
2020-07-27 12:36:07 +0200

    add local time to android result, history: replace start time (utc) with local time

--- android-app/src/main/java/at/alladin/nettest/nntool/android/app/workflow/measurement/MeasurementService.java
+++ android-app/src/main/java/at/alladin/nettest/nntool/android/app/workflow/measurement/MeasurementService.java
@@ -418,6 +418,7 @@ public class MeasurementService extends Service implements ServiceConnection {
             }
             reportDto.getTimeBasedResult().setStartTime(startDateTime);
             reportDto.getTimeBasedResult().setEndTime(endDateTime);
+            reportDto.setLocalTime(LocalDateTime.now());
 
             final SendReportTask task = new SendReportTask(mainActivity,
                     reportDto,
