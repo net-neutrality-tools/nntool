786ee3fb8b8483c829fbfcac9473e90a863ad4ff
Benjamin Pucher <bp@alladin.at>
2020-07-27 09:43:59 +0200

    Change UDP task to UDP_TURN task on website

--- nntool-typescript/libs/test/test.component.ts
+++ nntool-typescript/libs/test/test.component.ts
@@ -233,19 +233,18 @@ export class NetTestComponent extends BaseNetTestComponent implements OnInit {
     const qosMeasurementResult: QoSMeasurementResult = this.getCurrentQoSResult();
     for (const port of portBlockingTestResult.types[0].ports) {
       qosMeasurementResult.results.push({
-        udp_result_out_num_packets: port.packets.sent,
-        udp_result_out_rtt_avg_ns: port.delay ? port.delay.average_ns : null,
-        udp_result_out_rtts_ns: port.delays ? port.delays : null,
-        udp_result_out_delay_standard_deviation_ns: port.delay ? port.delay.standard_deviation_ns : null,
-        udp_objective_out_num_packets: port.packets.requested_packets,
+        udp_turn_result_num_packets: port.packets.sent,
+        udp_turn_result_rtt_avg_ns: port.delay ? port.delay.average_ns : null,
+        udp_turn_result_rtts_ns: port.delays ? port.delays : null,
+        udp_turn_result_delay_standard_deviation_ns: port.delay ? port.delay.standard_deviation_ns : null,
+        udp_turn_objective_num_packets: port.packets.requested_packets,
         qos_test_uid: port.uid,
-        test_type: 'udp',
-        udp_result_out_response_num_packets: port.packets.received,
-        udp_result_out_packet_loss_rate: port.packets.sent ? (port.packets.lost / port.packets.sent) * 100 : null,
-        udp_objective_out_port: port.number
+        test_type: 'udp_turn',
+        udp_turn_result_response_num_packets: port.packets.received,
+        udp_turn_result_packet_loss_rate: port.packets.sent ? (port.packets.lost / port.packets.sent) * 100 : null,
+        udp_turn_objective_port: port.number
       });
     }
-    
   }
 
   public tracerouteTestFinished(tracerouteTestResult: TracerouteTestState): void {
@@ -275,7 +274,6 @@ export class NetTestComponent extends BaseNetTestComponent implements OnInit {
       traceroute_objective_host: tracerouteTestResult.result.host,
       traceroute_result_hops: traceroute_result_details.length,
     });
-    
   }
 
   /**
@@ -283,7 +281,6 @@ export class NetTestComponent extends BaseNetTestComponent implements OnInit {
    * (either creates a new one or, if one was already created, returns the previously created one)
    */
   private getCurrentQoSResult(): QoSMeasurementResult {
-
     let qosResult: QoSMeasurementResult = undefined;
     if (this.testResults.length > 0) {
       for (let singleTest of this.testResults) {
@@ -360,14 +357,6 @@ export class NetTestComponent extends BaseNetTestComponent implements OnInit {
               return option.name === 'parameters_qos' ? option['measurement-parameters']['objectives'] : config;
               /* tslint:enable:no-string-literal */
             }, {});
-            //remove udp measurements not intended for the website
-            for (let i = 0; i < this.qosConfig.UDP.length; i++) {
-              let elem = this.qosConfig.UDP[i];
-              if (elem.allowed_on_web !== undefined && elem.allowed_on_web !== null && !elem.allowed_on_web) {
-                this.qosConfig.UDP.splice(i--, 1);
-              }
-            }
-
             this.qosControl = task;
             break;
         }
